a158bbc07efd6e1b30a8d89658a50ed3570188a8
Khiem H Nguyen <khnguyen88@gmail.com>
2022-11-10 19:46:31 -0500

    Added basic event listener for 1st start menu item

--- main.js
+++ main.js
@@ -1,10 +1,22 @@
 import { createScoreBoardElement } from "./modules/scoreboard.js";
 import { createCanvasElement } from "./modules/canvas.js";
 
+const startMenuItems = document.getElementById("startMenuItem1");
+startMenuItems.addEventListener("click", () => {
+  myFunction();
+});
+
+startMenuItems.addEventListener("click", myFunction);
+
+function myFunction() {
+  alert("Hello World!");
+}
+
 function init_elements() {
   console.log("hello, let's setup the file");
-  createScoreBoardElement();
-  createCanvasElement();
+
+  // createScoreBoardElement();
+  // createCanvasElement();
 }
 
 init_elements();
