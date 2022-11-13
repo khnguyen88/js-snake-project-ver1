72b7f11ec5330636ea70e63fb86082a5dc943602
Khiem H Nguyen <khnguyen88@gmail.com>
2022-11-13 17:03:19 -0500

    Added requestAnimationFrame loop w/ fixed FPS

--- main.js
+++ main.js
@@ -11,6 +11,12 @@ const inGameMenuContainer = document.getElementById("inGameMenuContainer");
 // All Initial Dom Elements, Objects, By Class Name, Array
 const startMenuItems = document.getElementsByClassName("startMenuItem");
 
+// Other global variables used to throttle framerate down from 60fps for requestAnimationbyFrame
+// Source code: https://stackoverflow.com/questions/19764018/controlling-fps-with-requestanimationframe
+var isAninimationOnFlag = true;
+var fps, fpsInterval, startTime, now, then, elapsed;
+fps = 1;
+
 // Event listener test code
 //----------------------------------------------------------------
 // Simple Event listner by id
@@ -50,6 +56,8 @@ function toggleDisplayBetweenTwoDOMElements(
   }
 }
 
+// Code to initialize elements
+//----------------------------------------------------------------
 function init_elements() {
   console.log("hello, let's setup the file");
 
@@ -57,4 +65,44 @@ function init_elements() {
   // createCanvasElement();
 }
 
+// Code to animate execute codes inside requestAnimationFrame method at a defined fixed FPS, default animate at 60fps
+// Source code:  https://stackoverflow.com/questions/19764018/controlling-fps-with-requestanimationframe
+//----------------------------------------------------------------
+// Code to initialize the timer variables and start the animation
+function startAnimation(fps) {
+  fpsInterval = 1000 / fps;
+  then = Date.now();
+  startTime = then;
+  console.log(startTime);
+  animate();
+}
+
+//  The animation loop calculates time elapsed since the last loop
+//  and only draws if your specified fps interval is achieved
+function animate() {
+  // Stop animation if check flag is off
+  console.log(isAninimationOnFlag);
+  if (!isAninimationOnFlag) {
+    return;
+  }
+
+  // Request another frame
+  requestAnimationFrame(animate);
+
+  // Calculate elapsed time since last loop
+  now = Date.now();
+  elapsed = now - then;
+
+  // If enough time has elapsed, draw the next frame
+  if (elapsed > fpsInterval) {
+    // Get ready for next frame by setting then=now, but also adjust for your
+    // specified fpsInterval not being a multiple of RAF's interval (16.7ms)
+    then = now - (elapsed % fpsInterval);
+
+    // draw stuff here
+    console.log("hi mom");
+  }
+}
+
 init_elements();
+startAnimation(fps);
